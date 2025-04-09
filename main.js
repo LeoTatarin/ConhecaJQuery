f9fe9bbaf7c18e658420ca901ee512e5afd31d7b
LeoTatarin <tatarinleo@gmail.com>
2025-04-08 23:09:06 -0300

    versao final da lista de tarefas

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
     $('header button').click(function() {
         $('form').slideDown();
@@ -23,3 +24,59 @@ $(document).ready(function(){
             $('#endereco-imagem-nova').val('')
     })
 })
+
+*/
+
+$(document).ready(function(){
+    
+    let tamanhoLista = 0;
+
+    $('#nova-tarefa').click(function() { // Quando clicar no botão nova-tarefa
+        $('#form-nova-tarefa').slideDown(); // Mostra o formulário form-nova-tarefa com efeito de deslizar pra baixo
+    });
+
+    $('#cancelar').click(function() { // Quando clicar no botão cancelar
+        $('#form-nova-tarefa').slideUp(); // Recolhe o formulário form-nova-tarefa no mesmo efeito
+    });
+
+    $('#adicionar').click(function(e){
+        e.preventDefault();
+        tamanhoLista++;
+        adicionarTarefa(tamanhoLista);
+    });
+
+
+});
+
+function adicionarTarefa(i){
+    const inputTarefa = $('#input-tarefa').val();
+    const inputData = $('#input-data').val();
+    const inputHora = $('#input-hora').val();
+    
+
+    const tarefaAdicionada = $(`
+        <li id="tarefa${i}">
+            ${inputData} -- ${inputHora} -- ${inputTarefa}
+            <button type="button" id="concluir${i}">Realizada</button>
+            <br>
+        </li>
+        
+    `);
+
+    $('#tarefas').append(tarefaAdicionada);
+
+    // Esconde o formulário
+    $('#form-nova-tarefa').slideUp();
+
+    // Limpa o formulário
+    $('#form-nova-tarefa')[0].reset();
+    
+    
+    $(`#concluir${i}`).click(function(){
+        let textoAnterior = $(`#tarefa${i}`).contents().first().text();
+        $(`#tarefa${i}`).html(`<span style="text-decoration: line-through; opacity: 0.5">${textoAnterior}</span><br>`);
+    });
+
+
+}
+
